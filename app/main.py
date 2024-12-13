ae7ec41f249ec011357bf1520a3947dbc323a1ec
Valerii Kashpur <kashpur.v.f@gmail.com>
2024-12-13 14:29:26 +0200

    [fix]: removed extra init

--- app/main.py
+++ app/main.py
@@ -38,9 +38,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
         self.create_powerful_api()
